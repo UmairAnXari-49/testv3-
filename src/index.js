c196bd236f8414d4e01f65dee818de899f4ea703
UmairAnXari-49 <93900172+UmairAnXari-49@users.noreply.github.com>
2023-12-30 20:17:48 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.js";
-import ContactSection from "./components/ContactSection/ContactSection.jsx";
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-
-ReactDOM.render(
+root.render(
   <React.StrictMode>
-    <ContactSection />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
-
-
-
