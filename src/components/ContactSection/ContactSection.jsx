273bed0fdc2479bfdb32494062819d4b12b74b88
UmairAnXari-49 <93900172+UmairAnXari-49@users.noreply.github.com>
2023-12-30 19:57:17 +0500

    Update ContactSection.jsx

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ContactSection.css";
 import backgroundVideo from "../../assets/backgroundVideo.mp4";
 
 function ContactSection() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    // Log the form data for demonstration purposes
+    console.log("Form submitted with data:", formData);
+
     // Additional logic for handling form submission, if needed
     // You can also use fetch or axios to send the form data to a server
-    console.log("Form submitted");
   };
 
   const handleSendButtonClick = () => {
@@ -39,13 +53,38 @@ function ContactSection() {
 
             <h1>Let's talk</h1>
 
-            <input type="email" name="email" placeholder="Email" required />
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
 
-            <input type="text" name="subject" placeholder="Subject" required />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+
+            <input
+              type="text"
+              name="subject"
+              placeholder="Subject"
+              value={formData.subject}
+              onChange={handleChange}
+              required
+            />
 
             <textarea
               name="message"
               placeholder="Your message"
+              value={formData.message}
+              onChange={handleChange}
               required
             ></textarea>
 
